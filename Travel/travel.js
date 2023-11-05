c6cd7137690f6f319e963cb02bfb3b46e252b81b
rjswh0503 <rjswh0503@yahoo.com>
2023-11-06 08:39:11 +0900

    html

--- Travel/travel.js
+++ Travel/travel.js
@@ -1,3 +1,25 @@
 document.getElementById("navyBtn1").onclick = function() {
     location.href = "LEGS.html";
 }
+
+
+
+
+
+
+
+
+
+let subToggle=true;
+$(".menu").click(()=> {
+    if(subToggle){
+        $(".sub").slideDown(1000);
+    }else{
+        $(".sub").slideUp(1000);
+    }
+    subToggle =! subToggle;
+});
+
+
+
+
